9e958d03e31b3c2fc7b6a8801714ba5bf406ba26
ashallowperson <245951141+ashallowperson@users.noreply.github.com>
2026-06-30 00:28:26 +0500

    CM6: remove unresolved AHardwareBuffer symbols
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol: AHardwareBuffer_allocate@LIBNATIVEWINDOW
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol:
    AHardwareBuffer_createFromHandle@LIBNATIVEWINDOW
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol: AHardwareBuffer_describe@LIBNATIVEWINDOW
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol:
    AHardwareBuffer_getNativeHandle@LIBNATIVEWINDOW
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol: AHardwareBuffer_lock@LIBNATIVEWINDOW
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol: AHardwareBuffer_release@LIBNATIVEWINDOW
    vendor/transsion/CM6/proprietary/vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so:
    error: Unresolved symbol: AHardwareBuffer_unlock@LIBNATIVEWINDOW

--- extract-files.py
+++ extract-files.py
@@ -139,13 +139,14 @@ blob_fixups: blob_fixups_user_type = {
 
     'odm/lib64/libTranssionTone.so': blob_fixup()
         .add_needed('libc++_shared.so'),
-    ('odm/lib64/libanc_single_rt_bokeh.so', 'vendor/lib64/libmcve.so', 'vendor/lib64/mt6789/libneuron_adapter_mgvi.so'): blob_fixup()
+    ('odm/lib64/libanc_single_rt_bokeh.so', 'vendor/lib64/libmcve.so', 'vendor/lib64/mt6789/libneuron_adapter_mgvi.so', 'vendor/lib64/libneuralnetworks_sl_driver_mtk_prebuilt.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
         .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle@LIBNATIVEWINDOW')
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_release')
-        .clear_symbol_version('AHardwareBuffer_unlock')
-        .clear_symbol_version('AHardwareBuffer_createFromHandle'),
+        .clear_symbol_version('AHardwareBuffer_unlock'),
 
 
 }  # fmt: skip
