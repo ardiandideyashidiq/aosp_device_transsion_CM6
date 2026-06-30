444048b70dfdd98b5190622e841539bf1d8419fe
rdndds <rdndds@gmail.com>
2026-06-30 08:48:10 +0700

    CM6: clean up dead code

--- extract-files.py
+++ extract-files.py
@@ -51,53 +51,7 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 
-def blob_fixup_return_1(
-    ctx: BlobFixupCtx,
-    file: File,
-    file_path: str,
-    symbol: str,
-    *args,
-    **kwargs,
-):
-    for line in run_cmd(
-        [
-            llvm_objdump_path,
-            '--dynamic-syms',
-            file_path,
-        ]
-    ).splitlines():
-        if line.endswith(f' {symbol}'):
-            offset, _ = line.split(maxsplit=1)
-
-            with open(file_path, 'rb+') as f:
-                f.seek(int(offset, 16))
-                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
-                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
-
-            break
-
-
 blob_fixups: blob_fixups_user_type = {
-    'vendor/app/Test.apk': blob_fixup()
-        .apktool_patch('blob-patches/TestApk.patch', '-s'),
-    'vendor/etc/test.conf': blob_fixup()
-        .patch_file('blob-patches/TestConf.patch')
-        .regex_replace('(LOG_.*_ENABLED)=1', '\\1=0')
-        .add_line_if_missing('DEBUG=0'),
-    ('vendor/etc/test.0.xml', 'vendor/etc/test.1.xml'): blob_fixup()
-        .fix_xml(),
-    'vendor/lib/test.so': blob_fixup()
-        .patchelf_version('0_17_2')
-        .fix_soname()
-        .add_needed('to_add.so')
-        .remove_needed('to_remove.so')
-        .replace_needed('from.so', 'to.so')
-        .clear_symbol_version('rpc_call_invoke')
-        .strip_debug_sections()
-        .binary_regex_replace(b'\xFF\x00\x00\x94', b'\xFE\x00\x00\x94')
-        .sig_replace('C0 03 5F D6 ?? ?? ?? ?? C0 03 5F D6', '1F 20 03 D5')
-        .call(blob_fixup_return_1, 'license_check'),
-
     'system_ext/bin/hw/android.hardware.audio.parameter_parser.service': blob_fixup()
         .replace_needed('android.hardware.audio.core-V3-ndk.so', 'android.hardware.audio.core-V4-ndk.so'),
     'vendor/lib64/mt6789/libmtkcam_hal_aidl_common.so': blob_fixup()
